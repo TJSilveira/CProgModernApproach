70c6a111fb6e8bb43e0336baec036ff5e29faa30
Tiago <tiagojesusilveira@gmail.com>
2025-03-05 19:57:30 +0100

    Changes in the function reminder
    This commit checks if the date is valid

--- EXchapter13/chapter13.c
+++ EXchapter13/chapter13.c
@@ -35,6 +35,11 @@ void reminder(){
         scanf("%2d",&day);
         if(day==0){
             break;
+        }else if(day>31||day<1){
+            printf("========================\n");
+            printf("%i is not a valid day\n",day);
+            printf("========================\n");
+            continue;
         }
         sprintf(day_str,"%2d",day);
         read_line(temp_reminder,MSG_LEN);
@@ -59,13 +64,86 @@ void reminder(){
     }
 }
 
+size_t strlen_ts(const char *s){
+    int i=0;
+
+    while(*s){
+        s++;
+        i++;
+    }
+
+    return i;
+}
+
+char *strcat_ts(char *s1,const char *s2){
+    char *temp_s=s1;
+
+    // Notice that *temp_s!='\0' is the same as *temp_s!=0, which can be simplified to *temp_s. 
+    while(*temp_s){
+        temp_s++;
+    }
+
+    // while(*s2!='\0'){
+    //     *temp_s=*s2;
+    //     temp_s++;
+    //     s2++;
+    // }
+    // *temp_s='\0';
+
+
+    // The previous loop can be further simplified.
+    // This is because, when you do an assignment inside a while, C checks the value that is being assigned as the value to use in the condition.
+    // This means that all values will be True ie !=0 other than 0, the last element of the array
+
+    while(*temp_s++=*s2++);
+
+    return s1;
+}
 
 void proj1(){
+    char small_word[60]="ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+    char largest_word[60]={0};
+    char temp_word[60];
 
+    while(1){
+        printf("Please insert word: ");
+        fgets(temp_word,60,stdin);
+        printf("%ld\n",strlen(temp_word));
+        if(strlen(small_word)>strlen(temp_word)){
+            strcpy(small_word,temp_word);
+        }
+
+        if(strlen(largest_word)<strlen(temp_word)){
+            strcpy(largest_word,temp_word);
+        }
+
+        if(strlen(temp_word)==5){
+            break;
+        }
+    }
+
+    printf("THE LARGEST WORD IS: %s",largest_word);
+    printf("the smallest word is: %s",small_word);
 }
 
 
-int main(){
-    reminder();
-    //proj2();
+int main(int argc, char *argv[]){
+    // reminder();
+    
+    // char array[] = "";
+    // printf("Resultado funcao strlen_ts: %li\n",strlen_ts(array));
+
+    // char array1[500]="abcdef";
+    // char array2[500]="ghijkl";
+
+    // printf("Result function strcat_ts: %s\n",strcat_ts(array1,array2));
+
+    // int i;
+
+    // for (i=0;i<argc;i++){
+    //     printf("%s\n",argv[i]);
+    // }
+
+    proj1();
+
 }
